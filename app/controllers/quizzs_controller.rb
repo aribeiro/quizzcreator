99e736bbcf31e85c84f8881e2e747556e03feab7
Alfredo Cipriani Oliveira Ribeiro <alfredo.engenharia@gmail.com>
2009-05-01 16:47:40 -0300

    quizz to quizzes

---
+++ app/controllers/quizzs_controller.rb
@@ -0,0 +1,12 @@
+class QuizzsController < ApplicationController
+  def index
+    @quizzs = Quizz.all
+  end
+
+  def new
+    @quizz = Quizz.new
+    @question_types = Question::QUESTION_TYPES
+    @question_weigth = Question::QUESTION_WEIGHT
+  end
+
+end
