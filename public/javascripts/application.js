d30f417102f98bd5a06bd4119f77b5387f49cdf8
Davis Zanetti Cabral <daviscabral@gmail.com>
2009-05-01 01:21:49 -0300

    i18n configuration, first layout, root controller and javascript to use nested attributes from rails 2.3

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,45 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Base = { }
+Base.Grid = Class.create({
+	initialize: function() {
+		var self = this;
+		$('grid_form').getElementsBySelector('.check_all').each(function(item) {
+			item.onclick = function() { self.checkAll(); }
+		});
+		$('grid_form').getElementsBySelector('.uncheck_all').each(function(item) {
+			item.onclick = function() { self.unCheckAll(); }
+		});		
+	},
+	
+	checkAll: function() {
+		this.check(true);		
+	},
+	
+	unCheckAll: function() {
+		this.check(false);
+	},
+	
+	check: function(option) {
+		$('grid_form').getElementsBySelector('input').each(function(item) {
+			item.checked = option;
+		});
+	}
+});
+
+Base.NestedAttributesJs = {  
+  add : function(e) {  
+    element = Event.findElement(e);  
+		object_id = element.up().up().href.replace(/.*#/, '');
+    template = eval(object_id + '_template');  
+    $(object_id).insert( { before: Base.NestedAttributesJs.replace_ids(template) } );  
+  },  
+  replace_ids : function(template){  
+    var new_id = new Date().getTime();  
+    return template.replace(/NEW_RECORD/g, new_id);  
+  }  
+}  
+  
+Event.observe(window, 'load', function(){  
+  $$('.add').each(function(link){
+    link.observe('click', Base.NestedAttributesJs.add);  
+  });  
+});
