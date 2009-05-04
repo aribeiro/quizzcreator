3b898d185db337dd9a435cd6a3138975b768986d
Alfredo Cipriani Oliveira Ribeiro <alfredo.engenharia@gmail.com>
2009-05-04 12:10:16 -0300

    Fixes to allow is_anwser equal false

--- app/models/multiple_choice.rb
+++ app/models/multiple_choice.rb
@@ -14,5 +14,5 @@
 class MultipleChoice < ActiveRecord::Base
   belongs_to :question
 
-  validates_presence_of :answer, :is_answer
+  validates_presence_of :answer
 end
--- test/unit/multiple_choice_test.rb
+++ test/unit/multiple_choice_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class MultipleChoiceTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   should_belong_to :question
-  should_require_attributes :answer, :is_answer 
+  should_require_attributes :answer
   
 end
