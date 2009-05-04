db0c9bf2c5b5e93f9ab96044bdf39b5e7f2142b6
Alfredo Cipriani Oliveira Ribeiro <alfredo.engenharia@gmail.com>
2009-05-04 10:47:43 -0300

    add js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,45 +1,44 @@
-Base = { }
-Base.Grid = Class.create({
-	initialize: function() {
-		var self = this;
-		$('grid_form').getElementsBySelector('.check_all').each(function(item) {
-			item.onclick = function() { self.checkAll(); }
-		});
-		$('grid_form').getElementsBySelector('.uncheck_all').each(function(item) {
-			item.onclick = function() { self.unCheckAll(); }
-		});		
-	},
-	
-	checkAll: function() {
-		this.check(true);		
-	},
-	
-	unCheckAll: function() {
-		this.check(false);
-	},
-	
-	check: function(option) {
-		$('grid_form').getElementsBySelector('input').each(function(item) {
-			item.checked = option;
-		});
-	}
-});
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+replace_ids = function(s){
+  var new_id = new Date().getTime();
+  return s.replace(/NEW_RECORD/g, new_id);
+}
 
-Base.NestedAttributesJs = {  
-  add : function(e) {  
-    element = Event.findElement(e);  
-		object_id = element.href.replace(/.*#/, '');
-    template = eval(object_id + '_template');  
-    $(object_id).insert( { before: Base.NestedAttributesJs.replace_ids(template) } );  
-  },  
-  replace_ids : function(template){  
-    var new_id = new Date().getTime();  
-    return template.replace(/NEW_RECORD/g, new_id);  
-  }  
-}  
-  
-Event.observe(window, 'load', function(){  
-  $$('.add').each(function(link){
-    link.observe('click', Base.NestedAttributesJs.add);  
-  });  
+var myrules = {
+  '.remove': function(e){
+    el = Event.findElement(e);
+    target = el.href.replace(/.*#/, '.')
+    el.up(target).hide();
+    if(hidden_input = el.previous("input[type=hidden]")) hidden_input.value = '1'
+  },
+  '.add_nested_item': function(e){
+    el = Event.findElement(e);
+    template = eval(el.href.replace(/.*#/, ''))
+    $(el.rel).insert({     
+      bottom: replace_ids(template)
+    });
+  },
+  '.add_nested_item_lvl2': function(e){
+    el = Event.findElement(e);
+    elements = el.rel.match(/(\w+)/g)
+    parent = '.'+elements[0]
+    child = '.'+elements[1]
+    
+    child_container = el.up(parent).down(child)    
+    parent_object_id = el.up(parent).down('input').name.match(/.*\[(\d+)\]/)[1]
+    
+    template = eval(el.href.replace(/.*#/, ''))
+
+    template = template.replace(/(attributes[_\]\[]+)\d+/g, "$1"+parent_object_id)
+    
+   // console.log(template)
+    child_container.insert({     
+      bottom: replace_ids(template)
+     });
+  }
+};
+
+Event.observe(window, 'load', function(){
+  $('container').delegate('click', myrules);
 });
